c9140e7309ce8d22a6fdcb195255c912373a9e23
gabsgc <gabriellagarccia@gmail.com>
2022-07-22 21:40:52 -0300

    feat: create project and initial settings

---
+++ src/test/java/br/com/alura/apilanguages/ApiLanguagesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.apilanguages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLanguagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
